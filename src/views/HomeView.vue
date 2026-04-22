8bcd47c0cde02b6306222b81bd02ce60b0c8d4e2
Juraci <juraci.vieira@gmail.com>
2026-04-22 08:30:38 -0300

    feat: wire CsvImport and StockDataTable into HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
+<!-- src/views/HomeView.vue -->
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
+import CsvImport from '@/components/CsvImport.vue'
+import StockDataTable from '@/components/StockDataTable.vue'
 </script>
 
 <template>
-  <main>
-    <HelloWorld msg="Hello World" />
+  <main class="p-4 flex flex-col gap-4">
+    <h1 class="text-2xl font-bold m-0">Price Matters</h1>
+    <CsvImport />
+    <StockDataTable />
   </main>
 </template>
